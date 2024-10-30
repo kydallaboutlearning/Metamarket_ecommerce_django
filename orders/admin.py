7afa99fc28e61962499edef4797ae480052f59f3
kydallaboutlearning <kydallaboutlearning@gmail.com>
2024-10-30 17:38:56 +0100

    added list filter for the order admin panel

--- orders/admin.py
+++ orders/admin.py
@@ -7,6 +7,31 @@ from .models import *
 
 #creating a modelinline admin
 
-class order(admin.TabularInline):
+class OrderItemInline(admin.TabularInline):
     model = OrderItems
     raw_id_fields = ['price','product','quantity']
+
+
+#creating a decorator to register the models
+@admin.register(Order)
+
+#creating an admin class for orders
+class OrderAdmin(admin.ModelAdmin):
+    """Creating a list display to show fields that would display in the admin panel"""
+    list_display = [
+        'first_name',
+        'last_name',
+        'email',
+        'address',
+        'postal_code',
+        'created',
+        'updated',
+        'city',
+        'paid',
+        ]
+    list_filter = [
+        'paid',
+        'created',
+        'updated',
+    ]
+    inlines = [OrderItemInline]
