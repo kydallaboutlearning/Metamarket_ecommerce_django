02390614492ed50bb22efc72204ace9c551befa3
kydallaboutlearning <kydallaboutlearning@gmail.com>
2024-10-31 01:28:45 +0100

    worked on the css, now on the SESSION_ID error

--- shop/views.py
+++ shop/views.py
@@ -21,6 +21,5 @@ def product_details(request,id,slug):
     product = get_object_or_404(Product,id=id,slug=slug,available=True)
     cart_product_form = CartAddProductForm()
     return render(request,'shop/product/detail.html',
-                  {'product':product},
-                  {'cart_product_form':cart_product_form}
+                  {'product':product,'cart_product_form':cart_product_form}
                   )
